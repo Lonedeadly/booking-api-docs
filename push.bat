ccdc573b303923aa253268b53041b0b566aace11
Lonedeadly <lonedeadly@yandex.ru>
2025-09-30 16:06:56 +0300

    update

--- push.bat
+++ push.bat
@@ -1,6 +1,17 @@
 @echo off
 cd /d %~dp0
+
+echo === Checking status ===
+git status
+
+echo === Adding all changes ===
 git add -A
+
+echo === Creating commit ===
 git commit -m "update"
-git push origin main --force
+
+echo === Pushing to GitHub ===
+git push origin main
+
+echo === Done! Refresh GitHub Pages in 1-3 minutes ===
 pause
